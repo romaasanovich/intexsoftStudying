ab91c45c0caedd8870d9de6a3df9aaa4f8b26b1c
Unknown <asanovich.roma@gmail.com>
2018-05-11 18:10:15 +0300

    resolve problem with path

--- bookservice/src/main/java/com/intexsoft/bookservice/controller/BookController.java
+++ bookservice/src/main/java/com/intexsoft/bookservice/controller/BookController.java
@@ -3,10 +3,7 @@ package com.intexsoft.bookservice.controller;
 import com.intexsoft.bookservice.api.BookService;
 import com.intexsoft.bookservice.entity.Book;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,14 +14,14 @@ public class BookController {
     @Autowired
     BookService bookService;
 
-    @GetMapping(name = "/books")
+    @GetMapping(path = "/books")
     public List<Book> getBooks() {
         return bookService.getAllBooks();
     }
 
 
-    @PostMapping(name = "/books")
-    public Book getBookBiId(@RequestParam(name = "id") Integer id) {
+    @GetMapping(path = "/books/{id}")
+    public Book getBookBiId(@PathVariable(name = "id") Integer id) {
         Optional<Book> book = bookService.getBookByID(id);
         return book.get();
     }
