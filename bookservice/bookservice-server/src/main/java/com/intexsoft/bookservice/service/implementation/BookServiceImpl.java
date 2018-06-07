b576bb1e4dd8f87c559eed4e85a2e2cdd4c2ebe7
Unknown <asanovich.roma@gmail.com>
2018-06-07 15:53:32 +0300

    fix problem with deleting book(doesn't delete reviews)

--- bookservice/bookservice-server/src/main/java/com/intexsoft/bookservice/service/implementation/BookServiceImpl.java
+++ bookservice/bookservice-server/src/main/java/com/intexsoft/bookservice/service/implementation/BookServiceImpl.java
@@ -1,8 +1,10 @@
 package com.intexsoft.bookservice.service.implementation;
 
 import com.intexsoft.bookservice.dao.entity.Book;
+import com.intexsoft.bookservice.dao.entity.Review;
 import com.intexsoft.bookservice.dao.repository.BookRepository;
 import com.intexsoft.bookservice.service.api.BookService;
+import com.intexsoft.bookservice.service.api.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +19,9 @@ public class BookServiceImpl implements BookService {
 
     @Autowired
     private BookRepository bookRepository;
+
+    @Autowired
+    private ReviewService reviewService;
 
     @Override
     public List<Book> getAllBooks() {
@@ -36,6 +41,9 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public void delete(Book book) {
+        for (Review review : reviewService.getBookReview(book.getId())) {
+            reviewService.delete(review);
+        }
         bookRepository.delete(book);
 
     }
