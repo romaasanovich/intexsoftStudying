a6d4da8910e9fa6d9ce16f6186c6e71b4354f28b
Unknown <asanovich.roma@gmail.com>
2018-06-11 11:44:52 +0300

    add passwords in comments

--- bookservice/bookservice-server/src/main/resources/db/migration/V10__addUsers.sql
+++ bookservice/bookservice-server/src/main/resources/db/migration/V10__addUsers.sql
@@ -1,3 +1,7 @@
+-- passes usingBCryptPasswordEncoder;
+-- pass root
 INSERT INTO user (login, password, email, role) VALUES ('root', '$2a$04$22r9pxCh/szFZ3HdLRpLGujn5UkiUZpQIRPa.7Yc0o.6mBALaVINy', 'qwerty@sm.re', 'ROLE_USER');
+-- pass 12345
 INSERT INTO user (login, password, email, role) VALUES ('cust1', '$2a$04$WNcCi/o6AR5KIKuJaroaaujKdAYGHzSYYfkWCjwdrtvpLva/UHbz6', 'user10@sm.re', 'ROLE_USER');
+-- pass 54321
 INSERT INTO user (login, password, email, role) VALUES ('cust', '$2a$04$6b9HcRHbAdGfJkCayuRfVu0AIoO8S1BbD3xOe1jA8U6LypOM2mFUe', 'user52@sm.re', 'ROLE_USER');
