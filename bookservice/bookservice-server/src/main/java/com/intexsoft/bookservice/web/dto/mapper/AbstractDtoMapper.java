d9e54c69590ec9120b624f085798d852732e0f74
Unknown <asanovich.roma@gmail.com>
2018-06-21 11:55:29 +0300

    implement mapping list using stream

--- bookservice/bookservice-server/src/main/java/com/intexsoft/bookservice/web/dto/mapper/AbstractDtoMapper.java
+++ bookservice/bookservice-server/src/main/java/com/intexsoft/bookservice/web/dto/mapper/AbstractDtoMapper.java
@@ -2,11 +2,19 @@ package com.intexsoft.bookservice.web.dto.mapper;
 
 import org.springframework.data.domain.Page;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public abstract class AbstractDtoMapper<Object, Dto> {
+
 
     public Page<Dto> toPageDto(Page<Object> objectPage) {
         Page<Dto> dtoPage = objectPage.map(this::toDto);
         return dtoPage;
+    }
+
+    public List<Dto> toDtoList(List<Object> objectList) {
+        return objectList.stream().map(this::toDto).collect(Collectors.toList());
     }
 
     public abstract Dto toDto(Object object);
